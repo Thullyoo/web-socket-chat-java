4e3383a6f62e14efec58576cc94685d7d9ca8a9e
Thullyoo <thullyofilho@gmail.com>
2024-12-18 13:49:53 -0300

    feat: add dto's

---
+++ src/main/java/br/thullyoo/web_socket_chat/dto/MessageInDTO.java
@@ -0,0 +1,4 @@
+package br.thullyoo.web_socket_chat.dto;
+
+public record MessageInDTO(String user, String message){
+}
---
+++ src/main/java/br/thullyoo/web_socket_chat/dto/MessageOutDTO.java
@@ -0,0 +1,4 @@
+package br.thullyoo.web_socket_chat.dto;
+
+public record MessageOutDTO(String message){
+}
