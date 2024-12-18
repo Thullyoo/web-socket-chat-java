32007b89e7840ebe2b6f2e54ae67a3c280cb667e
Thullyoo <thullyofilho@gmail.com>
2024-12-18 12:46:24 -0300

    Initial commit

---
+++ src/test/java/br/thullyoo/web_socket_chat/WebSocketChatApplicationTests.java
@@ -0,0 +1,13 @@
+package br.thullyoo.web_socket_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
